5c521ee06f68b166cf52a69f1b07fabcf3dc9cea
ben <9029708+nullcoalescence@users.noreply.github.com>
2023-12-02 19:02:06 -0600

    day selection

--- Program.cs
+++ Program.cs
@@ -2,7 +2,35 @@
 {
     static void Main(string[] args)
     {
-        var day1 = new Day1();
-        day1.Run();
+        Console.WriteLine("Advent of Code 2023");
+
+        // parse day
+        int day;
+
+        if (args.Length > 0)
+        {
+            day = int.Parse(args[0]);
+        }
+        else
+        {
+            Console.WriteLine("Enter day");
+            day = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"Press [ENTER] to run day #{day}");
+        Console.ReadLine();
+
+        // run day
+        switch (day)
+        {
+            case 1:
+                new Day1().Run();
+                break;
+            case 2:
+                break;
+            default:
+                Console.WriteLine("Invalid day");
+                break;
+        }
     }
 }
